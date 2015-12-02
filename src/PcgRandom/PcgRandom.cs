b3515b3f72971ca254e4b8b598471e036182576f
Bradley Grainger <bgrainger@gmail.com>
2015-12-01 20:47:31 -0800

    Add basic project structure.

---
+++ src/PcgRandom/PcgRandom.cs
@@ -0,0 +1,10 @@
+namespace PcgRandom
+{
+	/// <summary>
+	/// Implements the <code>pcg32</code> random number generator described
+	/// at <a href="http://www.pcg-random.org/using-pcg-c.html">http://www.pcg-random.org/using-pcg-c.html</a>.
+	/// </summary>
+	public sealed class PcgRandom32
+	{
+	}
+}
