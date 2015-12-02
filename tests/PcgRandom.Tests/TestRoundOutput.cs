ee42e25897911583b9d4accf884b495446187392
Bradley Grainger <bgrainger@gmail.com>
2015-12-02 09:29:42 -0800

    Add all official pcg32 tests.

---
+++ tests/PcgRandom.Tests/TestRoundOutput.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+namespace PcgRandom.Tests
+{
+    public sealed class TestRoundOutput
+    {
+		public TestRoundOutput(uint[] randomNumbers, string coins, int[] rolls, string cards)
+		{
+			RandomNumbers = randomNumbers;
+			Coins = coins.Select(x => x == 'H' ? 1 : 0).ToArray();
+			Rolls = rolls;
+			Cards = cards;
+		}
+
+	    public uint[] RandomNumbers { get; }
+
+	    public int[] Coins { get; }
+
+		public int[] Rolls { get; }
+
+		public string Cards { get; }
+	}
+}
